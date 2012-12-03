8e78572ec2ade116b8cd3dda74b251b91935da9a
Ori Livneh <ori@wikimedia.org>
2012-12-03 04:31:38 -0800

    Tweaks to JS module
    - Create a platform-independent mw.performance object so browser
      detection only has to happen once.
    - Use conventional MW immediate function invocation for module.
    - Use mw.log and emit output by default only in debug mode.
    
    Patrick: I stopped there with the hope that I could still somehow
    dissuade you from hashing / encoding the data, which strikes me as
    an odd choice. Can we discuss again?
    
    Change-Id: I1f63399aa7fb4c3068b3abf05197a833527c2348

--- modules/ext.navigationTiming.js
+++ modules/ext.navigationTiming.js
@@ -6,29 +6,30 @@
  * @author Patrick Reilly <preilly@wikimedia.org>
  */
 
-mw.navigationTiming = ( function( $ ) {
-function init() {
-	var performance, timing, navigation, timingBase64Str, navigationBase64Str, timingSHA1, navigationSHA1, eventString;
-	if ( typeof JSON !== 'undefined' ) {
-		performance = window.performance || window.mozPerformance || window.msPerformance || window.webkitPerformance || {};
-		timing = JSON.stringify( performance.timing || {} );
-		navigation = JSON.stringify( performance.navigation || {} );
-		timingBase64Str = $().crypt( { method:"b64enc",
-			source:timing } );
-		timingSHA1 = $().crypt( { method:"sha1",
-			source:timing } );
-		eventString = timingSHA1 + '::' +  timingBase64Str;
-		if ( window.console ) {
-			console.log( 'json: ' + timing );
-			console.log( 'json: ' + navigation );
-			console.log( 'json in base64: ' + timingBase64Str );
-			console.log( 'json sha1: ' + timingSHA1 );
-			console.log( 'event string: ' + eventString );
-		}
-	}
-}
+( function ( mw, $ ) {
+	var timing;
 
-return {
-	init: init
-};
-} )( jQuery );
+	var performance = mw.performance = { timing: {}, navigation: {}, memory: {} };
+	$.extend( performance, window.performance, window.mozPerformance,
+		window.msPerformance, window.webkitPerformance );
+
+	var timing, navigation, timingBase64Str, navigationBase64Str, timingSHA1,
+		navigationSHA1, eventString;
+
+	timing = JSON.stringify( performance.timing || {}, null, 2 );
+	navigation = JSON.stringify( performance.navigation || {}, null, 2 );
+	timingBase64Str = $().crypt( { method:"b64enc",
+		source:timing } );
+	timingSHA1 = $().crypt( { method:"sha1",
+		source:timing } );
+	eventString = timingSHA1 + '::' +  timingBase64Str;
+
+	if ( mw.config.get( 'debug' ) ) {
+		mw.log( 'json: ' + timing );
+		mw.log( 'json: ' + navigation );
+		mw.log( 'json in base64: ' + timingBase64Str );
+		mw.log( 'json sha1: ' + timingSHA1 );
+		mw.log( 'event string: ' + eventString );
+	}
+
+}( mediaWiki, jQuery ) );
