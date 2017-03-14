6f33cbc733dbe9a78dac18bc7ed205b1de9fcc58
Timo Tijhof <krinklemail@gmail.com>
2017-03-14 11:38:22 -0700

    Remove redundant 'return true' from PHP hooks
    Change-Id: I18313c92496b05dbc51870010eb9ff119c4c34c6

--- NavigationTiming.hooks.php
+++ NavigationTiming.hooks.php
@@ -1,16 +1,13 @@
 <?php
 
 class NavigationTimingHooks {
-
 	public static function onBeforePageDisplay( &$out, &$skin ) {
 		$out->addModules( 'ext.navigationTiming' );
-		return true;
 	}
 
 	public static function onResourceLoaderGetConfigVars( &$vars ) {
 		global $wgNavigationTimingSamplingFactor;
 		$vars[ 'wgNavigationTimingSamplingFactor' ] = $wgNavigationTimingSamplingFactor;
-		return true;
 	}
 
 	public static function onResourceLoaderTestModules( array &$modules, ResourceLoader &$rl ) {
