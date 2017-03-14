54d8b4232384442aa1170f0105136bc56e3ab7ed
Timo Tijhof <krinklemail@gmail.com>
2017-03-14 11:39:06 -0700

    ext.navigationTiming: Move hoisted functions before statements
    Makes code easier to follow. Especially with the early return
    being added in the next commit.
    
    Change-Id: I8c5bfe2aabf8a71fe7609ff918140a7efa2d4eb0

--- modules/ext.navigationTiming.js
+++ modules/ext.navigationTiming.js
@@ -12,37 +12,6 @@
 		visibilityChanged = false,
 		TYPE_NAVIGATE = 0;
 
-	if ( window.performance ) {
-		timing = performance.timing;
-		navigation = performance.navigation;
-	}
-
-	// Don't report measurements for pages that have loaded in the background.
-	// Browsers defer or deprioritize loading background pages, causing them to
-	// take longer to load, which throws off our measurements.
-	// See <https://phabricator.wikimedia.org/T146510#2794213> for more details.
-	if ( typeof document.hidden !== 'undefined' ) {
-		hiddenProp = 'hidden';
-		visibilityEvent = 'visibilitychange';
-	} else if ( typeof document.mozHidden !== 'undefined' ) {
-		hiddenProp = 'mozHidden';
-		visibilityEvent = 'mozvisibilitychange';
-	} else if ( typeof document.msHidden !== 'undefined' ) {
-		hiddenProp = 'msHidden';
-		visibilityEvent = 'msvisibilitychange';
-	} else if ( typeof document.webkitHidden !== 'undefined' ) {
-		hiddenProp = 'webkitHidden';
-		visibilityEvent = 'webkitvisibilitychange';
-	}
-	if ( hiddenProp ) {
-		$( document ).one( visibilityEvent, function () {
-			visibilityChanged = true;
-		} );
-		if ( document[ hiddenProp ] ) {
-			visibilityChanged = true;
-		}
-	}
-
 	function inSample() {
 		var factor = mw.config.get( 'wgNavigationTimingSamplingFactor' );
 		if ( !$.isNumeric( factor ) || factor < 1 ) {
@@ -256,6 +225,37 @@
 				}
 			}, 10 );
 		} );
+	}
+
+	if ( window.performance ) {
+		timing = performance.timing;
+		navigation = performance.navigation;
+	}
+
+	// Don't report measurements for pages that have loaded in the background.
+	// Browsers defer or deprioritize loading background pages, causing them to
+	// take longer to load, which throws off our measurements.
+	// See <https://phabricator.wikimedia.org/T146510#2794213> for more details.
+	if ( typeof document.hidden !== 'undefined' ) {
+		hiddenProp = 'hidden';
+		visibilityEvent = 'visibilitychange';
+	} else if ( typeof document.mozHidden !== 'undefined' ) {
+		hiddenProp = 'mozHidden';
+		visibilityEvent = 'mozvisibilitychange';
+	} else if ( typeof document.msHidden !== 'undefined' ) {
+		hiddenProp = 'msHidden';
+		visibilityEvent = 'msvisibilitychange';
+	} else if ( typeof document.webkitHidden !== 'undefined' ) {
+		hiddenProp = 'webkitHidden';
+		visibilityEvent = 'webkitvisibilitychange';
+	}
+	if ( hiddenProp ) {
+		$( document ).one( visibilityEvent, function () {
+			visibilityChanged = true;
+		} );
+		if ( document[ hiddenProp ] ) {
+			visibilityChanged = true;
+		}
 	}
 
 	// Ensure we run after loadEventEnd.
