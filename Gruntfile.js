ddeda775513e0c35d4b655446a096db8b64ba5f2
Timo Tijhof <krinklemail@gmail.com>
2019-01-08 15:34:28 -0800

    Remove grunt-jsonlint
    jsonlint seems redundant given that our json files are already
    interpreted in a more meaningful way (not just syntax) by other
    tools:
    - i18n: banana-checker
    - package: npm
    - composer: composer
    - extension: mediawiki
    
    Change-Id: Ie5b9316bad027a85f2683f642507095a23da38ee

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,12 @@
 /* eslint-env node */
 module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
-	grunt.loadNpmTasks( 'grunt-jsonlint' );
 
 	grunt.initConfig( {
-		jsonlint: {
-			all: [
-				'**/*.json',
-				'!node_modules/**',
-				'!vendor/**'
-			]
-		},
 		banana: {
 			all: 'i18n/'
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'banana' ] );
 };
