cad07e9583a340dcf40f62fd4c79b80b57b82e92
zoranzoki21 <zorandori4444@gmail.com>
2020-09-12 12:54:45 +0200

    Fix MediaWiki.Commenting.FunctionComment.MissingDocumentationPublic
    I also removed "exclude-patterns" from .phpcs.xml which are already default.
    
    Change-Id: I9245c107c4a45420df9a0acb827b005314563c66

--- includes/NavigationTimingHooks.php
+++ includes/NavigationTimingHooks.php
@@ -1,6 +1,10 @@
 <?php
 
 class NavigationTimingHooks {
+	/**
+	 * @param OutputPage &$out
+	 * @param Skin &$skin
+	 */
 	public static function onBeforePageDisplay( &$out, &$skin ) {
 		$out->addModules( 'ext.navigationTiming' );
 	}
